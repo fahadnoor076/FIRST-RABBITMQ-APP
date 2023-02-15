c8b2886b0e037c3075e7a39df43617bc2a35e66b
Fahad Noor <fahadnoor076@gmail.com>
2023-02-15 14:50:47 +0800

    Add project

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+
+var consumer = new EventingBasicConsumer(channel);
+
+var random = new Random();
+
+consumer.Received += (model, ea) =>
+{
+    var processingTime = random.Next(1, 6);
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received: {message} will take {processingTime} to process");
+    Task.Delay(TimeSpan.FromSeconds(processingTime));
+};
+
+channel.BasicConsume(queue: "letterbox", 
+                    autoAck: false, // means we need to manually acknowledge the queue that message received/processed
+                    consumer: consumer);
+
+Console.ReadKey();
---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+
+string message = "Faisal Here";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
